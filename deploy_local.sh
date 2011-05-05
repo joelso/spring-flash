b37bdb8d24f3bcb45159ee0c0986ca391e912a7c
joelso <joel.soderstrom@gmail.com>
2011-05-05 08:23:33 +0200

    new snapshot: spring-flash

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,1 +1,5 @@
-mvn -DaltDeploymentRepository=snapshot-repo::default::file:../joelso-mvn-repo/snapshots clean deploy
+mvn -DaltDeploymentRepository=snapshot-repo::default::file:../joelso-mvn-repo/snapshots clean deploy;
+git --exec-path=../joelso-mvn-repo/ add .;
+git --exec-path=../joelso-mvn-repo/ commit -m 'new snapshot: spring-flash';
+git --exec-path=../joelso-mvn-repo/ push;
+
