6fc9f9838e2854d4a4a7b9cd21a5c28411096902
joelso <joel.soderstrom@gmail.com>
2011-05-05 09:05:01 +0200

    removed deploy script from repo

--- deploy_local.sh
+++
@@ -1,5 +0,0 @@
-mvn -DaltDeploymentRepository=snapshot-repo::default::file:../joelso-mvn-repo/snapshots clean deploy;
-git --exec-path=../joelso-mvn-repo/ add .;
-git --exec-path=../joelso-mvn-repo/ commit -m 'new snapshot: spring-flash';
-git --exec-path=../joelso-mvn-repo/ push;
-
